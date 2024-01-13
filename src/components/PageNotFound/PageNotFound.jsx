1b1f2692f2c87e72546dc5cd186404b10940326f
feras Moh <ferawwwess@gmail.com>
2024-01-13 09:54:01 +0300

    news

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,13 +1,16 @@
 import { Button } from "react-bootstrap";
 import "./PageNotFound.scss";
+import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
     <div className="page-not-found">
       <h1>:( URL NOT FOUND :(</h1>
       <h4>Sorry we can't find this url in our website</h4>
-      <Button variant="warning" href="/News/all-news" className="page-button">
-        Back To News
-      </Button>
+      <Link to="/News/all-news">
+        <Button variant="warning" className="page-button">
+          Back To News
+        </Button>
+      </Link>
     </div>
   );
 };
